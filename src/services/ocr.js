d74010deea4a72801f2e5efb1ac4b078ab89ea0a
avadhesh11 <202452309@iiitvadodara.ac.in>
2026-01-09 11:10:49 +0530

    oshrer

--- src/services/ocr.js
+++ src/services/ocr.js
@@ -2,7 +2,7 @@ import {createWorker} from "tesseract.js";
 
 export const scanBill = async(imageBuffer) => {
     const worker = await createWorker('eng');
-    const ret = await worker.recognize(imageBuffer);
+    const ret = await worker.recognize(imageBuffer  );
     const dt = ret.data.text;
     await worker.terminate();
     return dt;
