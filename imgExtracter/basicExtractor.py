a9593eb32a38ddda45913b4835ce8ba39f4204fe
tarundeepakjain <tarundeepakjain@gmail.com>
2026-01-09 20:20:08 +0530

    Remove Redundant data

--- imgExtracter/basicExtractor.py
+++
@@ -1,9 +0,0 @@
-#Simple Text Extraction
-
-from PIL import Image
-import pytesseract
-
-image = Image.open("testBill.png")
-text = pytesseract.image_to_string(image)
-
-print("Extracted Text:",text)
